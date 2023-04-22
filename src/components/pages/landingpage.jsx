b5e1b7325827f7cd3579ec9b43342eec16af19e6
MartinK <82144965+RaikunGZ@users.noreply.github.com>
2023-04-22 08:00:13 +0200

    delete feedback container

--- src/components/pages/landingpage.jsx
+++ src/components/pages/landingpage.jsx
@@ -1,5 +1,4 @@
 import { TopCarousel } from "../landingpage_assets/carousel"
-import { FeedbackContainer } from "../landingpage_assets/feedbackContainer"
 import { FirstArticle } from "../landingpage_assets/firstArticle"
 import { SecondArticle } from "../landingpage_assets/secondArticle"
 
@@ -7,7 +6,6 @@ export const Landingpage = () => {
     return(
         <section>
             <TopCarousel id={0}/>
-            <FeedbackContainer />
             <FirstArticle />
             <SecondArticle />
         </section>
